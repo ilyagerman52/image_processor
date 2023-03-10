75f6083287d1ed9959bc822ebd1824355f03cf65
Ilya German <91181813+ilyagerman52@users.noreply.github.com>
2023-03-10 16:49:38 +0300

    Delete image_processor directory

--- image_processor/image_data/colour.cpp
+++
@@ -1,9 +0,0 @@
-#include "colour.h"
-
-Colour::Colour() = default;
-Colour::Colour(float r, float g, float b) : r_(r), g_(g), b_(b){};
-Colour::Colour(const Colour& other_colour) : r_(other_colour.r_), g_(other_colour.g_), b_(other_colour.b_){};
-Colour::~Colour() = default;
-
-
-
--- image_processor/reading_and_writing/reader.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <string>
-#include "../image_data/image.h"
-
-void SaveBmp(const std::string& path, const Image& image);
-Image ReadBmp(const std::string& path);
